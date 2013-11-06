d121ea717bf4d5ef19be77d1825b9574ac43a202
Jiang Youxin <jiangyouxin@360.cn>
2013-11-06 11:28:13 +0800

    ActionBar

--- src/com/fantuan/MainActivity.java
+++ src/com/fantuan/MainActivity.java
@@ -1,10 +1,11 @@
 package com.fantuan;
 
 import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 
 import com.googlecode.androidannotations.annotations.*; 
 
 @EActivity(R.layout.main)
-public class MainActivity extends Activity {
+public class MainActivity extends ActionBarActivity {
 }
