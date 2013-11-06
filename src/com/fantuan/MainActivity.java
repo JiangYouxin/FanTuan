089923092aacf350c0fb9a9235d7db5b7fb3dc9d
Jiang Youxin <jiangyouxin@360.cn>
2013-11-06 12:36:13 +0800

    添加新成员对话框

--- src/com/fantuan/MainActivity.java
+++ src/com/fantuan/MainActivity.java
@@ -1,9 +1,11 @@
 package com.fantuan;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.googlecode.androidannotations.annotations.*; 
 
@@ -18,5 +20,16 @@ public class MainActivity extends ActionBarActivity {
         getSupportFragmentManager().beginTransaction()
                 .add(android.R.id.content, f)
                 .commit();
+    }
+
+    @OptionsItem
+    void menu_add() {
+        new AlertDialog.Builder(this)
+            .setTitle(R.string.input_name)
+            .setIcon(android.R.drawable.ic_dialog_info)
+            .setView(new EditText(this))
+            .setPositiveButton(R.string.ok, null)
+            .setNegativeButton(R.string.cancel, null)
+            .show();
     }
 }
