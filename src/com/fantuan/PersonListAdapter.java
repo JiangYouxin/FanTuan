20b80b6cf3926f1d465a0bf736fb4b06b5e9d434
Jiang Youxin <jiangyouxin@360.cn>
2013-11-07 10:04:50 +0800

    双行LIST

--- src/com/fantuan/PersonListAdapter.java
+++ src/com/fantuan/PersonListAdapter.java
@@ -2,23 +2,67 @@ package com.fantuan;
 
 import com.fantuan.model.Person;
 
-import android.widget.ArrayAdapter;
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
 
 import com.googlecode.androidannotations.annotations.*;
 
 @EBean
-public class PersonListAdapter extends ArrayAdapter<Person> {
+public class PersonListAdapter extends BaseAdapter {
     @Bean
     FanTuanManager mFanTuanManager;
 
-    public PersonListAdapter(Context context) {
-        super(context, android.R.layout.simple_list_item_1);
+    @RootContext
+    Context mContext;
+
+    @Override
+    public Object getItem(int position) {
+        return mFanTuanManager.getPersonList().get(position);
     }
 
-    @AfterInject
+    @Override
+    public int getCount() {
+        return mFanTuanManager.getPersonList().size();
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup container) {
+        ViewHolder viewHolder;
+        if (convertView == null) {
+            convertView = LayoutInflater.from(mContext).inflate(
+                   android.R.layout.simple_list_item_2, null);
+            viewHolder = new ViewHolder(convertView);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder)convertView.getTag();
+        }
+        viewHolder.setModel((Person)getItem(position));
+        return convertView;
+    }
+
     public void refresh() {
-        clear();
-        addAll(mFanTuanManager.getPersonList());
+        notifyDataSetChanged();
+    }
+
+    private static class ViewHolder {
+        private TextView text1;
+        private TextView text2;
+        public ViewHolder(View view) {
+            text1 = (TextView) view.findViewById(android.R.id.text1);
+            text2 = (TextView) view.findViewById(android.R.id.text2);
+        }
+        public void setModel(Person p) {
+            text1.setText(p.name);
+            text2.setText(String.format("%.2f", p.current));
+        }
     }
 }
