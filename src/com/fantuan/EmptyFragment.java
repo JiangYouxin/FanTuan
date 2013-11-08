714ee37b09213487e250794359ac924e046aecab
Jiang Youxin <jiangyouxin@360.cn>
2013-11-08 17:56:42 +0800

    Bottom Bar

---
+++ src/com/fantuan/EmptyFragment.java
@@ -0,0 +1,9 @@
+package com.fantuan;
+
+import android.support.v4.app.ListFragment;
+
+import com.googlecode.androidannotations.annotations.*; 
+
+@EFragment
+public class EmptyFragment extends ListFragment {
+}
--- src/com/fantuan/MainActivity.java
+++ src/com/fantuan/MainActivity.java
@@ -1,8 +1,6 @@
 package com.fantuan;
 
-import android.app.Activity;
-import android.support.v4.app.Fragment;
-import android.support.v7.app.ActionBar;
+import android.support.v4.app.FragmentTabHost;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 
@@ -10,24 +8,31 @@ import com.googlecode.androidannotations.annotations.*;
 
 @EActivity(R.layout.main)
 public class MainActivity extends ActionBarActivity {
-    @Bean
-    FanTuanManager mFanTuanManager;
-
-    @Bean
-    TabsAdapter mAdapter;
+    @ViewById
+    FragmentTabHost tab_host;
 
     @AfterViews
     void init() {
-        ActionBar bar = getSupportActionBar();
-        bar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-        mAdapter.addTab(bar.newTab().setText(R.string.person_list_title),
-                MainListFragment_.class, null);
-        mAdapter.addTab(bar.newTab().setText(R.string.history_list_title),
-                HistoryListFragment_.class, null);
+        tab_host.setup(this, getSupportFragmentManager(), R.id.realtabcontent);
+        tab_host.addTab(
+                tab_host.newTabSpec("person").setIndicator(getString(R.string.person_list_title)),
+                MainListFragment_.class,
+                null);
+        tab_host.addTab(
+                tab_host.newTabSpec("history").setIndicator(getString(R.string.history_list_title)),
+                HistoryListFragment_.class,
+                null);
+        tab_host.addTab(
+                tab_host.newTabSpec("discover").setIndicator(getString(R.string.discover_title)),
+                EmptyFragment_.class,
+                null);
+        tab_host.addTab(
+                tab_host.newTabSpec("more").setIndicator(getString(R.string.more_title)),
+                EmptyFragment_.class,
+                null);
     }
 
     @Override
     public void onSaveInstanceState(Bundle state) {
     }
-
 }
